890937403aca501910643990f99779541cda3f6f
Ayush <ayushrvekaria@gmail.com>
2024-02-06 19:39:57 +0530

    added routes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const rootRouter = require("./routes/index");
+const cors = require('cors')
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors())
+app.use(express.json())
+app.use("/api/v1", rootRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3000,()=>{
+  console.log("app listening on port 3000");
 })
