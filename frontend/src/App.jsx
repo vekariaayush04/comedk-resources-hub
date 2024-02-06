3f98782cd8927334e48ed5753a1cc32bb297d983
mant0597 <123318369+mant0597@users.noreply.github.com>
2024-02-06 22:37:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { About, Home, Login, Register } from "./components/pages";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
