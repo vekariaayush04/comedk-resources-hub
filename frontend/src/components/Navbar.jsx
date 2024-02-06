4e72db4f0e4b450d3beecdbe06bed8c9b1ae1088
Ayush <ayushrvekaria@gmail.com>
2024-02-07 00:15:57 +0530

    edited navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,28 @@
-import React,{useState} from 'react'
-import {Link,NavLink} from 'react-router-dom'
-import './Navbar.css'
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css";
 const Navbar = () => {
-const [menuOpen, setMenuOpen] = useState(false);
-  return <nav>
-    <Link to="/" className="title">
-        ComedSark
-      </Link>
-      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      {/* <div></div> */}
-    <ul className={menuOpen ? "open" : ""}>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/about'>About</NavLink></li>
-        <li><NavLink to='/register'>Register</NavLink></li>
-        <li><NavLink to='/login'>Login</NavLink></li>
-    </ul>
-  </nav>
-}
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <nav>
+
+      <h1 className="title">ComedSark</h1>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Register</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
--- frontend/src/components/pages/Register.jsx
+++ frontend/src/components/pages/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <h1>Register</h1>
   )
 }
 
