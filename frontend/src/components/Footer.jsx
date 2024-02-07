b9aa5ec2667d8e7e582ca41c5dc3ff16035bd0e1
mant0597 <123318369+mant0597@users.noreply.github.com>
2024-02-07 13:54:00 +0530

    adding css in index.css for footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1 className="text-center">All Right Reserved &copy; ComedSark</h1>
-      <p className="text-center mt-3">
+      <h1 className="footer-head">All Right Reserved &copy; ComedSark</h1>
+      <p className="footer-link">
         <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
         <Link to="/policy">Privacy Policy</Link>
       </p>
